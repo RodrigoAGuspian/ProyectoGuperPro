72868ab8046f6b6d5a5436a32a71ef5f494d7dc0
RodrigoAGuspian <37385496+RodrigoAGuspian@users.noreply.github.com>
2018-06-06 23:22:54 -0500

    commit9

--- ProyectoSena/ProyectoSena/urls.py
+++ ProyectoSena/ProyectoSena/urls.py
@@ -25,9 +25,7 @@ urlpatterns = [
 
 ]
 
-path = '/home/path/to/project'
-if path not in sys.path:
-    sys.path.append(path)
+
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
